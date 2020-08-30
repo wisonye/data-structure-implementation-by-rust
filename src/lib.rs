7fd993e0d2c9bce472c75ba15ae790748d03996e
Wison Ye <wisonye@gmail.com>
2020-08-30 20:12:17 +1200

    [ master ] - 'SingleLinkedList.push_to_end()' works.

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,10 @@
+//! [`SingeLinkedList`]
+//!
+//! [`SingeLinkedList`]: ../src/data_structure_implementation_by_rust/single_linked_list.rs.html
+
 mod single_linked_list;
 
 #[cfg(test)]
 mod tests {
     use crate::single_linked_list::SingeLinkedList;
-
-    #[test]
-    fn it_works() {
-        assert_eq!(2 + 2, 4);
-    }
 }
--- src/single_linked_list.rs
+++ src/single_linked_list.rs
@@ -9,141 +9,147 @@
 use std::fmt;
 use std::ptr::NonNull;
 
-/// The type here describes either a **valid pointer** or
-/// a **null pointer**. That's why we use `Option` here
-/// which only got 2 possible values:
+/// The type here describes either a **valid pointer** or a **null pointer**.
+/// That's why we use `Option` here which only got 2 possible values:
 ///
-/// `Some()` - **valid pointer**
-/// `None` - **null pointer**
+/// `Some()` - means **valid pointer**
+/// `None` - means **null pointer**
 ///
 /// The **valid pointer* means:
 ///
-/// 1. Non null, it must point to some `Node<T>` instance.
+/// 1. Non null, it must point to particular `Node<T>` instance.
 /// 2. `Node<T>` instance should live inside the **heap**.
 ///
-/// That's why we use `Box<Node<T>>` which is a smart pointer
-/// in **Rust**. For getting back a `NonNull<Node<T>>` type,
-/// we can use:
+/// That's why we use `Box<Node<T>>` which is a smart pointer in `Rust`.
+/// Below is the steps to get back a `NonNull<Node<T>>` from `Box<Node<T>>`:
 ///
 /// ```
-/// // let temp_node: Box<Node<T>> = Box::new(Node {});
-/// // let non_null_ptr = NonNull::from(Box::leak(temp_node));
+/// // 1. Create new box node instance
+/// // let box_node = Box::new(Node {});
+///
+/// // 2. Get back a `&mut Node<T>` from box node instance
+/// // let box_node_mut_ref: &mut Node<T> = Box::leak(box_node);
+///
+/// // 3. Get back `NonNull<Node<T>>` from `&mut Node<T>` (mutable reference just pointer)
+/// // let non_null_node = NonNull::from(box_node_mut_ref);
+///
+/// // 4. Finally, get back `Option<NonNull<Node<T>>>`
+/// // let head = Some(non_null_node);
+///
+/// // 5. All in one step:
+/// // let head = Some(NonNull::from(Box::leak(Box::new(Node {}))));
+/// ```
+///
+/// But how to get back `Option<&Node<T>>` from `Option<NonNull<Node<T>>>`???
+///
+/// `NonNull<Node<T>>.as_ptr()` get back a raw pointer to `Node<T>`, then We got 2 ways to do that:
 ///
 /// ```
+/// // let mut next_option_ref: Option<&Node<T>> = match self.next {
+/// //     Some(temp_next) => {
+/// //         let ptr: *mut Node<T> = temp_next.as_ptr();
+/// //         unsafe { Some(&*ptr) }
+/// //     }
+/// //     None => None,
+/// // };
+///
+/// // let mut next_option_ref: Option<&Node<T>> = None;
+/// // if self.next.is_some() {
+/// //     let unwrap_non_null = self.next.as_ref().unwrap();
+/// //     let raw_ptr: *mut Node<T> = unwrap_non_null.as_ptr();
+/// //     unsafe {
+/// //         next_option_ref = Some(&*raw_ptr);
+/// //     }
+/// // }
+/// ```
+///
+/// By the way, we **CANNOT** use `Some(Box::from_raw(temp_head.as_ptr()))` to get back a
+/// `Option<Box<Node<T>>>`, as that will consume and drop the node instance when it out of scope
+/// which will cause `pointer being freed was not allocated`!!!
+///
 type PointToNextNode<T> = Option<NonNull<Node<T>>>;
 
+// ------------------------- Node<T> ---------------------------------
+
 ///
-pub struct Node<T: Clone + Copy + fmt::Debug> {
+struct Node<T: Clone + fmt::Debug> {
     data: T,
     next: PointToNextNode<T>,
 }
 
-impl<T: Clone + Copy + fmt::Debug> fmt::Debug for Node<T> {
+impl<T: Clone + fmt::Debug> fmt::Debug for Node<T> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        let mut next_option_ref: Option<&Node<T>> = match self.next {
+            Some(temp_next) => {
+                // Get back raw pointer to `Node<T>` from `NonNull<Node<T>>`
+                let ptr: *mut Node<T> = temp_next.as_ptr();
+                unsafe { Some(&*ptr) }
+            }
+            None => None,
+        };
+
         f.debug_struct("Node")
             .field("data", &self.data)
-            .field("next", &self.next)
+            .field("next", &next_option_ref)
             .finish()
     }
 }
 
-impl<T: Clone + Copy + fmt::Debug> Node<T> {
+impl<T: Clone + fmt::Debug> Node<T> {
     pub fn get_data(&self) -> &T {
         &self.data
     }
-
-    // pub fn get_next(&self) -> &Option<NonNull<T>> {
-    // &self.next
-    // }
 }
 
+// ------------------------- SingeLinkedList<T> ----------------------
+
 ///
-pub struct SingeLinkedList<T: Clone + Copy + fmt::Debug> {
+pub struct SingeLinkedList<T: Clone + fmt::Debug> {
     size: usize,
     head: PointToNextNode<T>,
     tail: PointToNextNode<T>,
 }
 
-impl<T: Clone + Copy + fmt::Debug> fmt::Debug for SingeLinkedList<T> {
+impl<T: Clone + fmt::Debug> fmt::Debug for SingeLinkedList<T> {
     fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
-        unsafe {
-            // `self.head.as_ref()` get back `Option<&NonNull<Node<T>>>`
-            // `self.head.as_ref().unwrap()` get back `&NonNull<Node<T>>`
-            // `self.head.as_ref().unwrap().as_ptr()` get back `*mut Node<T>` (raw pointer to Node<T>)
-            let head_ptr_to: *mut Node<T> = self.head.as_ref().unwrap().as_ptr();
+        let mut head_option_ref: Option<&Node<T>> = match self.head {
+            Some(temp_head) => {
+                // Get back raw pointer to `Node<T>` from `NonNull<Node<T>>`
+                let ptr: *mut Node<T> = temp_head.as_ptr();
+                unsafe { Some(&*ptr) }
+            }
+            None => None,
+        };
 
-            // Cast back to `Box<Node<T>>` from raw pointer
-            let temp_head = Some(Box::from_raw(head_ptr_to));
+        let mut tail_option_ref: Option<&Node<T>> = match self.tail {
+            Some(temp_tail) => {
+                // Get back raw pointer to `Node<T>` from `NonNull<Node<T>>`
+                let ptr: *mut Node<T> = temp_tail.as_ptr();
+                unsafe { Some(&*ptr) }
+            }
+            None => None,
+        };
 
-            let tail_ptr_to: *mut Node<T> = self.tail.as_ref().unwrap().as_ptr();
-            // let temp_tail = Some(Box::from_raw(tail_ptr_to));
-
-            f.debug_struct("SingeLinkedList")
-                .field("size", &self.size)
-                .field("head", &temp_head)
-                // .field("tail", &temp_tail)
-                .finish()
-        }
+        f.debug_struct("SingeLinkedList")
+            .field("size", &self.size)
+            .field("head", &head_option_ref)
+            .field("tail", &tail_option_ref)
+            .finish()
     }
 }
 
-impl<T: Clone + Copy + fmt::Debug> SingeLinkedList<T> {
-    pub fn new(data: T) -> Self {
-        let head_and_tail: Box<Node<T>> = Box::new(Node { data, next: None });
-        let non_null_raw_ptr = NonNull::from(Box::leak(head_and_tail));
-
+impl<T: Clone + fmt::Debug> SingeLinkedList<T> {
+    pub fn new() -> Self {
         SingeLinkedList {
-            size: 1,
-            head: Some(non_null_raw_ptr),
-            tail: Some(non_null_raw_ptr),
+            size: 0,
+            head: None,
+            tail: None,
         }
     }
 
     pub fn get_size(&self) -> usize {
         self.size
     }
-
-    // pub fn get_head(&self) -> &Node<T> {
-    // &self.head
-    // }
-
-    // fn has_child_node(&self, node_to_check: &Node<T>) -> bool {
-    // let head_without_child = self.head.next.is_none();
-    // if head_without_child == true {
-    // return false;
-    // }
-    //
-    // let mut has_child_for_the_node = false;
-    // let mut next_node: &Node<T> = self.head.next.as_ref().unwrap();
-    // let mut walk_through_size = 1;
-    // while walk_through_size <= self.size {
-    // // Compare to see whether the same `&Node<T>`
-    // if ptr::eq(next_node, node_to_check) {}
-    //
-    // walk_through_size += 1;
-    // }
-    //
-    // has_child_for_the_node
-    // }
-
-    // pub fn add_to_end(&mut self, data: T) {
-    // let head_without_child = self.head.next.is_none();
-    // if head_without_child == true {
-    // self.head.next = Some(Box::new(Node { data, next: None }));
-    // self.size += 1;
-    // // println!("Attached data to 'self.head'.");
-    // return;
-    // }
-    //
-    // let mut next_node: &mut Node<T> = self.head.next.as_mut().unwrap();
-    // let mut walk_through_size = 1;
-    // while next_node.next.is_some() {
-    // next_node = next_node.next.as_mut().unwrap();
-    // }
-    //
-    // next_node.next = Some(Box::new(Node { data, next: None }));
-    // self.size += 1;
-    // }
 
     pub fn pop_front(&mut self) -> Option<T> {
         if self.size <= 0 {
@@ -160,18 +166,30 @@ impl<T: Clone + Copy + fmt::Debug> SingeLinkedList<T> {
     pub fn push_to_end(&mut self, data: T) {
         // tail --> New Node (become new `tail`)
 
-        // Create new tail node
-        let new_tail_node = Box::new(Node { data, next: None });
-        let new_tail_node_ptr = NonNull::from(Box::leak(new_tail_node));
+        // Create new box wrap the node instance
+        let new_tail_node_box = Box::new(Node { data, next: None });
+
+        // Convert `Box<Node<T>>` to `NonNull<Node<T>>`
+        let new_tail_node_ptr = NonNull::from(Box::leak(new_tail_node_box));
+
+        // Get back `Option<NonNull<Node<T>>>`
+        let new_tail_node = Some(new_tail_node_ptr);
 
         // `self.tail.next` is always `None`, so just reset it no need to worry about anything.
         unsafe {
-            let tail_ptr_to = self.tail.unwrap().as_ptr();
-            (*tail_ptr_to).next = Some(new_tail_node_ptr);
+            match self.tail {
+                Some(temp_tail) => {
+                    // `temp_tail` is `NonNull<Node<TT>>`, here we get back the raw pointer
+                    let raw_ptr: *mut Node<T> = temp_tail.as_ptr();
+                    // let tail_node_instance: Node<T> = *raw_ptr;
+                    (*raw_ptr).next = new_tail_node;
+                }
+                None => self.head = new_tail_node,
+            }
         }
 
         // Reset `self.tail`
-        self.tail = Some(new_tail_node_ptr);
+        self.tail = new_tail_node;
         self.size += 1;
     }
 }
@@ -182,22 +200,22 @@ mod tests {
 
     #[test]
     fn create_empty_list() {
-        let empty_list = SingeLinkedList::new("Wison Ye");
+        let empty_list = SingeLinkedList::<String>::new();
         println!("empty_list: {:#?}", &empty_list);
 
-        assert_eq!(empty_list.get_size(), 1);
-        println!("empty_list: {:#?}", &empty_list);
-        // assert_eq!(empty_list.get_head().get_data(), &"Wison Ye");
-        // assert_eq!(empty_list.get_head().get_next().is_none(), true);
+        assert_eq!(empty_list.get_size(), 0);
+        assert_eq!(empty_list.head.is_none(), true);
+        assert_eq!(empty_list.tail.is_none(), true);
     }
 
-    // #[test]
-    // fn push_to_end_should_work() {
-    // let mut test_list = SingeLinkedList::new("Wison Ye");
-    // test_list.push_to_end("Fion Li");
-    // // test_list.push_to_end("Mike Ye");
-    //
-    // println!("test_list: {:#?}", test_list);
-    // assert_eq!(test_list.get_size(), 3);
-    // }
+    #[test]
+    fn push_to_end_should_work() {
+        let mut test_list = SingeLinkedList::new();
+        test_list.push_to_end("Rust".to_owned());
+        test_list.push_to_end("is".to_owned());
+        test_list.push_to_end("Awesome".to_owned());
+
+        println!("test_list: {:#?}", &test_list);
+        assert_eq!(test_list.get_size(), 3);
+    }
 }
