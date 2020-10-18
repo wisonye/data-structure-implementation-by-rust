3d9bd0f7bf9a86e6d415b11250cdeb1d49991330
Wison Ye <wisonye@gmail.com>
2020-10-19 12:49:12 +1300

    [ master ] - 'Stack' added 'peek()' and make all functions public.

--- src/stack.rs
+++ src/stack.rs
@@ -1,7 +1,7 @@
 use std::collections::LinkedList;
 use std::fmt;
 
-// ------------------------- Stack<T> First-In-First-Out-------------
+// ------------------------- Stack<T> Last-in-first-out -------------
 
 ///
 #[derive(Debug)]
@@ -13,7 +13,7 @@ pub struct Stack<T: Clone + fmt::Debug> {
 ///
 impl<T: Clone + fmt::Debug> Stack<T> {
     ///
-    fn new() -> Self {
+    pub fn new() -> Self {
         Stack {
             size: 0usize,
             inner_list: LinkedList::new(),
@@ -21,12 +21,12 @@ impl<T: Clone + fmt::Debug> Stack<T> {
     }
 
     ///
-    fn len(&self) -> usize {
+    pub fn len(&self) -> usize {
         self.size
     }
 
     ///
-    fn pop(&mut self) -> Option<T> {
+    pub fn pop(&mut self) -> Option<T> {
         let result = self.inner_list.pop_back();
         if result.is_some() {
             self.size -= 1;
@@ -36,13 +36,28 @@ impl<T: Clone + fmt::Debug> Stack<T> {
     }
 
     ///
-    fn push(&mut self, data: T) {
+    pub fn peek(&mut self) -> Option<T> {
+        // As the `LinkedList` doesn't implement the `peek`, then we need
+        // to `pop` first and copy and then `push` back again
+        let temp_pop = self.inner_list.pop_back();
+        if temp_pop.is_some() {
+            let original = temp_pop.unwrap();
+            let copy = original.clone();
+            self.inner_list.push_back(original);
+            Some(copy)
+        } else {
+            None
+        }
+    }
+
+    ///
+    pub fn push(&mut self, data: T) {
         self.inner_list.push_back(data);
         self.size += 1;
     }
 
     ///
-    fn is_empty(&self) -> bool {
+    pub fn is_empty(&self) -> bool {
         self.size == 0
     }
 }
@@ -59,7 +74,7 @@ mod tests {
     }
 
     #[test]
-    fn push_and_pop_should_work() {
+    fn should_work_with_primitive_type() {
         let mut my_stack = Stack::<u8>::new();
         my_stack.push(10);
         assert_eq!(false, my_stack.is_empty());
@@ -78,11 +93,20 @@ mod tests {
         assert_eq!(pop_value, Some(20));
         println!("my_stack {:#?}", &my_stack);
 
+        let peek_value = my_stack.peek();
+        assert_eq!(peek_value, Some(10));
+        assert_eq!(1, my_stack.len());
+
         pop_value = my_stack.pop();
         assert_eq!(true, my_stack.is_empty());
         assert_eq!(0, my_stack.len());
         assert_eq!(pop_value, Some(10));
         println!("my_stack {:#?}", &my_stack);
+
+        // Empty
+        assert_eq!(true, my_stack.is_empty());
+        assert_eq!(None, my_stack.peek());
+        assert_eq!(None, my_stack.pop());
     }
 
     #[test]
@@ -96,11 +120,11 @@ mod tests {
         let mut book_stack = Stack::<Book>::new();
         book_stack.push(Book {
             title: "No 1 sales",
-            author: "Nobody"
+            author: "Nobody",
         });
         book_stack.push(Book {
             title: "Homes",
-            author: "NZ publish"
+            author: "NZ publish",
         });
         assert_eq!(false, book_stack.is_empty());
         assert_eq!(2, book_stack.len());
@@ -109,11 +133,18 @@ mod tests {
         assert_eq!(true, book.is_some());
         assert_eq!("Homes", book.as_ref().unwrap().title);
 
+        let peek = book_stack.peek();
+        assert_eq!(true, peek.is_some());
+        assert_eq!("No 1 sales", peek.as_ref().unwrap().title);
+        println!("book_stack {:#?}", &book_stack);
+
         book = book_stack.pop();
         assert_eq!(true, book.is_some());
         assert_eq!("No 1 sales", book.as_ref().unwrap().title);
 
+        // Empty
         assert_eq!(true, book_stack.is_empty());
+        assert_eq!(true, book_stack.peek().is_none());
         assert_eq!(true, book_stack.pop().is_none());
     }
 }
